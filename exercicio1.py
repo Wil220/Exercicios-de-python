007de9ed4697e777698a22c78c49877e72aa0157
Wil220 <wilker.silva0714@gmail.com>
2024-01-10 23:11:00 -0300

    exercico de python1

---
+++ exercicio1.py
@@ -0,0 +1,4 @@
+nome=input('qual seu nome?')
+idade=input('qual sua idade?')
+cidade=input('qual cidade voce mora?')
+print('prazer ', nome,' voce mora em',cidade,'e tem ',idade,'anos, seja bem vindo(a)!')
