0862842b2185da5b8ae73f402dd368ddf2bcd577
Wil220 <wilker.silva0714@gmail.com>
2024-01-12 19:02:30 -0300

    exerciciopython

--- exercicio6.py
+++ exercicio6.py
@@ -2,4 +2,4 @@ n=int(input('digite um numero?'))
 d=n*2
 t=n*3
 r=n**(1/2)
-print('o dobro de ',n, 'é' ,d, 'o triplo é' ,t ,'a raizquadra é', r)
+print('o dobro de ',n, 'é' ,d, 'o triplo é' ,t ,'a raiz quadrada é', r)
