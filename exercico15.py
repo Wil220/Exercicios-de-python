2d719ef24b82702e3afd01ea3921b78a9242d039
Wil220 <wilker.silva0714@gmail.com>
2024-01-17 20:22:13 -0300

    exerciciopython

--- exercico15.py
+++ exercico15.py
@@ -1,3 +1,4 @@
 grauC = float(input('qual á temperura da sua região: ?'))
-grauF = (grauC * 9/ 5) +32
-print(' a temperatura de {}celsius para fahrenheit é {}fahrenheit'.format(grauC, grauF ))
+grauF = 31.5
+(grauC * 9/ 5) +32
+print(' a temperatura de {} celsius para fahrenheit é {} fahrenheit'.format(grauC, grauF ))
