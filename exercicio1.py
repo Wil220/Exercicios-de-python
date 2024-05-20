83c1e91cd4ee954b03d1ad8308f2b2300a20f3fe
Wil220 <wilker.silva0714@gmail.com>
2024-05-20 17:31:40 -0300

    ajuste

--- exercicio1.py
+++ exercicio1.py
@@ -2,3 +2,5 @@ nome=input('qual seu nome?')
 idade=input('qual sua idade?')
 cidade=input('qual cidade voce mora?')
 print('prazer ', nome,' voce mora em',cidade,'e tem ',idade,'anos, correto?, seja bem vindo(a)!')
+
+print('eu')
